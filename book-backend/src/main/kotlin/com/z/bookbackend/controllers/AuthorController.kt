976a857bc0cc196614952a4934d4d6ba6c59fe98
Mariano <m_villa@hotmail.com>
2018-08-05 21:01:50 -0300

    api docs

--- book-backend/src/main/kotlin/com/z/bookbackend/controllers/AuthorController.kt
+++ book-backend/src/main/kotlin/com/z/bookbackend/controllers/AuthorController.kt
@@ -2,6 +2,7 @@ package com.z.bookbackend.controllers
 
 import com.z.bookbackend.models.Author
 import com.z.bookbackend.services.AuthorDAO
+import com.z.bookbackend.util.BasicCrud
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
@@ -10,10 +11,32 @@ import java.util.*
 
 @RestController
 @RequestMapping("api/author")
-class AuthorController(private val authorDAO: AuthorDAO) {
-    @GetMapping fun getAll(pageable: Pageable): Page<Author> = authorDAO.findAll(pageable)
+class AuthorController(private val authorDAO: AuthorDAO):BasicCrud<String,Author> {
 
-    @GetMapping("{id}") fun getById(@PathVariable id:String): Optional<Author> = authorDAO.findById(id)
+    @GetMapping override fun getAll(pageable: Pageable): Page<Author> = authorDAO.findAll(pageable)
 
-    @PostMapping fun insert(@RequestBody author: Author): Author = authorDAO.insert(author)
+    @GetMapping("{id}") override fun getById(@PathVariable id:String): Optional<Author> = authorDAO.findById(id)
+
+    @PostMapping override fun insert(@RequestBody obj: Author): Author{
+        return if(obj.id.isNullOrBlank()){
+            authorDAO.insert(obj)
+        }else{
+            throw object : Exception("ID must be null or empty"){}
+        }
+    }
+
+
+    @PutMapping override fun update(obj: Author): Author {
+        return if(obj.id.isNullOrBlank()){
+            throw object : Exception("ID must not be null or empty"){}
+        }else{
+            authorDAO.insert(obj) //TODO: update all books of the updated author
+        }
+    }
+
+    @DeleteMapping override fun deleteById(id: String): Optional<Author> {
+        return authorDAO.findById(id).apply {
+            this.ifPresent { authorDAO.delete(it) } //TODO: delete all books of the deleted author
+        }
+    }
 }
--- book-backend/src/main/kotlin/com/z/bookbackend/controllers/BookController.kt
+++ book-backend/src/main/kotlin/com/z/bookbackend/controllers/BookController.kt
@@ -2,6 +2,7 @@ package com.z.bookbackend.controllers
 
 import com.z.bookbackend.models.Book
 import com.z.bookbackend.services.BookDAO
+import com.z.bookbackend.util.BasicCrud
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.*
@@ -10,10 +11,19 @@ import java.util.*
 
 @RestController
 @RequestMapping("api/book")
-class BookController (private val bookDAO: BookDAO){
-    @GetMapping fun getAll(pageable: Pageable): Page<Book> = bookDAO.findAll(pageable)
+class BookController (private val bookDAO: BookDAO): BasicCrud<String, Book> {
 
-    @GetMapping("{isbn}") fun getByISBN(@PathVariable isbn:String): Optional<Book> = bookDAO.findById(isbn)
+    @GetMapping override fun getAll(pageable: Pageable): Page<Book> = bookDAO.findAll(pageable)
 
-    @PostMapping fun insert(@RequestBody book: Book): Book = bookDAO.insert(book)
+    @GetMapping("{id}") override fun getById(@PathVariable id:String): Optional<Book> = bookDAO.findById(id)
+
+    @PostMapping override fun insert(@RequestBody obj: Book): Book = bookDAO.insert(obj)
+
+    @PutMapping override fun update(@RequestBody obj: Book): Book = bookDAO.insert(obj) //TODO: re-insert author from db to avoid inconsistency
+
+    @DeleteMapping("{id}") override fun deleteById(id: String): Optional<Book> {
+        return bookDAO.findById(id).apply {
+            this.ifPresent { bookDAO.delete(it) }
+        }
+    }
 }
